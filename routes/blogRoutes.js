a9a2672b9d9ed611de3760ef61339d09b900796b
Hamzat Ajanaku <hajanaku@dlm.group>
2023-06-30 13:32:16 +0100

    heroku

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,13 +1,13 @@
 const multer = require("multer");
 const { Router } = require("express");
 const { createPost, createCategory, getCategories } = require("../controllers/postController");
-const { fileUploadMiddleware } = require("../utils/fileUploadMiddleware");
-const { uploadMultiple } = require("../utils/uploadMultiple");
+// const { fileUploadMiddleware } = require("../utils/fileUploadMiddleware");
+// const { uploadMultiple } = require("../utils/uploadMultiple");
 const upload = multer({ dest: "./temp" }).single("photo");
 
 const router = Router();
 
-router.route("/").post(upload, uploadMultiple, createPost);
+router.route("/").post(createPost);
 router.route("/category").get(getCategories).post(createCategory);
 
 module.exports = router;
